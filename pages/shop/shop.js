611ea391ddbc71dfe8cf9a53bd8da9d1e214e8a5
zouge666 <1615379639@qq.com>
2021-09-07 21:58:33 +0800

    0907

--- pages/shop/shop.js
+++ pages/shop/shop.js
@@ -11,7 +11,23 @@ Page({
       {bindtap:'Menu1',txt:'添加商品'},
       {bindtap:'Menu2',txt:'删除商品'}
     ],
-    menu:''
+    menu:'',
+    active: 0,
+    scrollTop:0,
+    currentIndex: 0,
+    leftMenuList:['饭类','面类','肉类','菜类'],
+    rightContent:[
+      {id:1, goods_url:"https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fdealer0.autoimg.cn%2Fdl%2F122602%2Fnewsimg%2F130512469295591733.jpg&refer=http%3A%2F%2Fdealer0.autoimg.cn&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1632643953&t=2fe554793f120713303cfe5f685ec244",goods_name:"鸡腿", goods_price:5, goods_detail:'这是一个鸡腿，它是怎么怎么怎么做的饿，非常好吃哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈', num:0},
+      {id:2, goods_url:"https://img1.baidu.com/it/u=2201661351,1806193528&fm=26&fmt=auto&gp=0.jpg",goods_name:"烤全鸡", goods_price:35, goods_detail:'这是一只烤全鸡，它是怎么怎么怎么做的饿，非常好吃哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈', num:0},
+      {id:3, goods_url:"https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fimg.mp.itc.cn%2Fupload%2F20170430%2F51ad7cf754794174a0825437a92f50b1_th.jpeg&refer=http%3A%2F%2Fimg.mp.itc.cn&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1632994493&t=b63962784517ee50c3be623d2201e217", goods_name:"炒饭", goods_price:13, goods_detail:'这是炒饭，它是怎么怎么怎么做的饿，非常好吃哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈', num:0}
+    ],
+    bgc: 'gray',
+    order:{
+      goods:[],
+      totalPrice: 0,
+      totalNum: 0,
+      date: ''
+    }
   },
   CATES:[],
   actionSheetTap:function(){
